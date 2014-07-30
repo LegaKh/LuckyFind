8a6f75d990505c2d1c460997c78a4f4c54f55e25
Oleg Kariuk <olegkariuk@gmail.com>
2014-07-30 17:43:44 +0300

    comment profile route

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe UsersController, :type => :controller do
 
   describe "GET profile" do
-    it "returns http success" do
-      get :profile
-      expect(response).to be_success
-    end
+    #it "returns http success" do
+      #get :profile
+      #expect(response).to be_success
+    #end
   end
 
 end
