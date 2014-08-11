c039ef9254517193a5e02df6e8cba60c9375d932
Oleg <LegaKh@gmail.com>
2014-08-11 10:10:53 +0300

    password recovery 1

--- app/models/doc.rb
+++ app/models/doc.rb
@@ -2,5 +2,5 @@ class Doc < ActiveRecord::Base
   has_one :ad, as: :content, dependent: :destroy
   has_one :user, through: :ad
 
-  validates :doc_type, :doc_code, :first_name, :last_name, presense: true, length: { in: 2..22 }
+  validates :doc_type, :doc_code, :first_name, :last_name, presence: true, length: { in: 2..22 }
 end
