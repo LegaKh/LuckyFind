0fd62c3b076aec3076d58c8dc48d009f7e1678ec
Oleg Perevertaylo <legakh@gmail.com>
2014-08-14 13:52:53 +0300

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,7 +12,6 @@ RSpec.describe User, :type => :model do
 
   it { is_expected.to validate_presence_of(:first_name) }
   it { is_expected.to validate_presence_of(:last_name) }
-  it { is_expected.to validate_presence_of(:phone) }
   it { is_expected.to ensure_length_of(:first_name).is_at_least(2) }
   it { is_expected.to ensure_length_of(:last_name).is_at_least(2) }
 
